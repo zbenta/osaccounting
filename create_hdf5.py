191e81a2ff531a75fba13214958363a57c78d416
mariojmdavid <mariojmdavid@gmail.com>
2017-08-08 11:12:12 +0100

    test if

--- create_hdf5.py
+++ create_hdf5.py
@@ -194,12 +194,14 @@ if __name__ == '__main__':
                 print 5*'<'
                 print 'Date= ', to_isodate(ts[i])
                 for u in usg:
-                    if u["state"] != "error":
-                        a_vcpus[i] = a_vcpus[i] + u["vcpus"]
-                        a_mem_mb[i] = a_mem_mb[i] + u["memory_mb"]
-                        a_disk_gb[i] = a_disk_gb[i] + u["local_gb"]
-                        print 'uvcpus= ',  u["vcpus"], ' a_vcpus[i]= ', a_vcpus[i]
-                        print 2*'_'
+                    if u["state"] == "error":
+                        print 'ooo THIS is the IF in state error: ', u["state"]
+                        continue
+                    a_vcpus[i] = a_vcpus[i] + u["vcpus"]
+                    a_mem_mb[i] = a_mem_mb[i] + u["memory_mb"]
+                    a_disk_gb[i] = a_disk_gb[i] + u["local_gb"]
+                    print 'u[state]= ', u["state"], ' uvcpus= ',  u["vcpus"], ' a_vcpus[i]= ', a_vcpus[i]
+                    print 2*'_'
 
             res = grp.create_dataset('date', data=ts)
             res = grp.create_dataset('vcpus', data=a_vcpus)
