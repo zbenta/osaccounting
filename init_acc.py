eb98b7ca113b760a94c1becc23570ca0add95f6b
mariojmdavid <mariojmdavid@gmail.com>
2017-08-10 16:50:39 +0100

    trial of new version

--- init_acc.py
+++ init_acc.py
@@ -35,16 +35,27 @@ if __name__ == '__main__':
             for inst in instances:
                 t_create = to_secepoc(inst["created_at"])
                 t_final = ts[size_a-1]
-                if vol["deleted"]:
-                    t_final = to_secepoc(vol["deleted_at"])
+                if inst["deleted_at"]:
+                    t_final = to_secepoc(inst["deleted_at"])
                 if t_final > to_secepoc(datetime.datetime.utcnow()):
                     t_final = to_secepoc(datetime.datetime.utcnow())
                 idx_start, idx_end = dt_to_indexes(t_create, t_final, year)
-                p = filter(lambda pr: pr['ID'] == vol['project_id'], projects)
+                p = filter(lambda pr: pr['ID'] == inst['project_id'], projects)
                 proj = p[0]
                 grp_name = proj['Name']
-                vol_array = f[grp_name]['volume_gb']
-                vol_array[idx_start:idx_end] = vol_array[idx_start:idx_end] + vol['size']
+                vcpu_array = f[grp_name]['vcpus']
+                vcpu_array[idx_start:idx_end] = vcpu_array[idx_start:idx_end] + inst['vcpus']
+                mem_array = f[grp_name]['mem_mb']
+                mem_array[idx_start:idx_end] = mem_array[idx_start:idx_end] + inst['memory_mb']
+                disk_array = f[grp_name]['disk_gb']
+                disk_array[idx_start:idx_end] = disk_array[idx_start:idx_end] + inst['root_gb']
+                print 80 * '-'
+                print 'xxxxxxxxxxx   INSTANCES   xxxxxxxxxxxxxxxxxxxxxxxx'
+                print "ProjID= ", inst['project_id'], " VCPUs= ", inst['vcpus'], "  --ProjID from filter= ", proj
+                print 'IDX_Start= ', idx_start, ' IDX_End= ', idx_end
+                print 'Inst Start= ', to_isodate(t_create), ' Inst End= ', to_isodate(t_final)
+                print 'VCPU Array Start= ', vcpu_array[idx_start], ' VCPU Array End= ', vcpu_array[idx_end - 1]
+                print 'TS_Start= ', ts[idx_start], ' TS_End= ', ts[idx_end]
 
             # This block is to insert the values from Cinder volumes
             volumes = get_volumes(year)
@@ -61,34 +72,10 @@ if __name__ == '__main__':
                 grp_name = proj['Name']
                 vol_array = f[grp_name]['volume_gb']
                 vol_array[idx_start:idx_end] = vol_array[idx_start:idx_end] + vol['size']
-
                 print 80*'-'
+                print 'xxxxxxxxxxx   VOLUMES   xxxxxxxxxxxxxxxxxxxxxxxx'
                 print "ProjID= ", vol['project_id'], " SizeGB= ", vol['size'], "  --ProjID from filter= ", proj
                 print 'IDX_Start= ', idx_start, ' IDX_End= ', idx_end
                 print 'Vol_Start= ', to_isodate(t_create), ' Vol_End= ', to_isodate(t_final)
                 print 'VolArray_Start= ', vol_array[idx_start], ' VolArray_End= ', vol_array[idx_end-1]
                 print 'TS_Start= ', ts[idx_start], ' TS_End= ', ts[idx_end]
-
-
-
-""" This block uses the nova API bindings
-                nova = get_nova_client(proj['Name'])
-
-                for i in range(sa):
-                    aux = nova.usage.get(proj['ID'], to_isodate(ts[i]), to_isodate(ts[i]+DELTA))
-                    usg = getattr(aux, "server_usages", [])
-                    #print 5*'>'
-                    #print 'index= ', i, ' DATE= ', to_isodate(ts[i]), 'EPOCH= ', ts[i]
-                    #pprint.pprint(usg)
-                    #print 5*'<'
-                    for u in usg:
-                        if u["state"] == "error":
-                            continue
-                        a_vcpus[i] = a_vcpus[i] + u["vcpus"]
-                        a_mem_mb[i] = a_mem_mb[i] + u["memory_mb"]
-                        a_disk_gb[i] = a_disk_gb[i] + u["local_gb"]
-                        #print 'u[state]= ', u["state"], ' uvcpus= ',  u["vcpus"], ' a_vcpus[i]= ', a_vcpus[i]
-                        #print 2*'_'
-"""
-
-
