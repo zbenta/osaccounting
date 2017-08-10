6ebfd9d647eec15797dd1b9e51c8b973d5590e56
mariojmdavid <mariojmdavid@gmail.com>
2017-08-10 14:18:02 +0100

    fill volume array

--- init_volumes.py
+++ init_volumes.py
@@ -34,17 +34,15 @@ if __name__ == '__main__':
                 if t_final > to_secepoc(datetime.datetime.utcnow()):
                     t_final = to_secepoc(datetime.datetime.utcnow())
                 idx_start, idx_end = dt_to_indexes(t_create, t_final, year)
-                proj = filter(lambda pr: pr['ID'] == vol['project_id'], projects)
+                p = filter(lambda pr: pr['ID'] == vol['project_id'], projects)
+                proj = p[0]
+                grp_name = proj['Name']
+                vol_array = f[grp_name]['volume_gb']
+                vol_array[idx_start:idx_end] = vol_array[idx_start:idx_end] + vol['size']
+
                 print 80*'-'
                 print "ProjID= ", vol['project_id'], " SizeGB= ", vol['size'], "  --ProjID from filter= ", proj
                 print 'IDX_Start= ', idx_start, ' IDX_End= ', idx_end
                 print 'Vol_Start= ', to_isodate(t_create), ' Vol_End= ', to_isodate(t_final)
+                print 'VolArray_Start= ', vol_array[idx_start], ' VolArray_End= ', vol_array[idx_end-1]
                 print 'TS_Start= ', ts[idx_start], ' TS_End= ', ts[idx_end]
-
-            #for proj in projects:
-                # print 20*'-'
-                # print proj
-            #    grp_name = proj['Name']
-            #    vol_array = f[grp_name]['volume_gb']
-            #    for i in range(size_a):
-            #        vol_array[i] = i*2
