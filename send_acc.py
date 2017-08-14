420361148466c20fd9fdda1640b7f47f4bf2b403
mariojmdavid <mariojmdavid@gmail.com>
2017-08-14 14:04:30 +0100

    create send acc

---
+++ send_acc.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2017 LIP
+#
+# Author: Mario David <mariojmdavid@gmail.com>
+#
+
+
+"""Send accounting data to graphite
+"""
+
+from osacc_functions import *
+
+if __name__ == '__main__':
+    years = get_years()
+    for year in years:
+        print 80 * "="
+        filename = get_hdf_filename(year)
+        print " Filename = ", filename
+        with h5py.File(filename, 'r') as f:
+            ti = f.attrs['LastRun']
+            ts = f['date'][:]
+            for group in f:
+                print group
+
