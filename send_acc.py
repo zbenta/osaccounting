9dad13c87e559607274438179268218177a1754f
mariojmdavid <mariojmdavid@gmail.com>
2017-08-16 13:53:29 +0100

    test pickle send

--- send_acc.py
+++ send_acc.py
@@ -17,7 +17,7 @@ from osacc_functions import *
 if __name__ == '__main__':
     env = get_env()
     carbon_server = env['carbon_server']
-    carbon_port = 2003
+    carbon_port = int(env['carbon_port'])
     # years = get_years()
     years = [2017]
     delay = 10  # 10 seconds delay
@@ -28,30 +28,33 @@ if __name__ == '__main__':
         with h5py.File(filename, 'r') as f:
             ti = f.attrs['LastRun']
             ts = f['date'][:]
-            # graph_list = []
             for group in f:
                 if group == "date":
                     continue
                 # print "--> Group = ", group
+                graph_list = []
                 for m in METRICS:
                     # print "--> Metric = ", m
                     data = f[group][m]
                     metric_str = GRAPH_NS + "." + group + "." + m
-                    # for i in range(323970, 323999):
-                    for i in range(50):
-                        sock = socket.socket()
-                        sock.connect((carbon_server, carbon_port))
+                    for i in range(326819, 326979):
+                    # for i in range(50):
+                        # sock = socket.socket()
+                        # sock.connect((carbon_server, carbon_port))
                         graph_string = metric_str + " " + str(data[i]) + " " + str(int(ts[i])) + "\n"
-                        sock.sendall(graph_string)
-                        sock.close()
-                        # graph_ds = (metric_str, (int(ts[i]), data[i]))
-                        # graph_list.append(graph_ds)
+                        # sock.sendall(graph_string)
+                        # sock.close()
+                        graph_ds = (metric_str, (int(ts[i]), data[i]))
+                        graph_list.append(graph_ds)
 
-            # pprint.pprint(graph_list)
-            # package = pickle.dumps(graph_list, protocol=2)
-            # size = struct.pack('!L', len(package))
-            # message = size + package
-            # sock.sendall(message)
-            # time.sleep(delay)
+                # pprint.pprint(graph_list)
+                package = pickle.dumps(graph_list, protocol=2)
+                size = struct.pack('!L', len(package))
+                message = size + package
+                sock = socket.socket()
+                sock.connect((carbon_server, carbon_port))
+                sock.sendall(message)
+                sock.close()
+                # time.sleep(delay)
 
 
