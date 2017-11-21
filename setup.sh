3baf988403ee4db0c6d1363fc5f7487a3a3f8323
mariojmdavid <mariojmdavid@gmail.com>
2017-11-21 13:40:52 +0000

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cp init_acc.py osacc_functions.py send_acc.py update_acc.py /usr/local/bin/
-cp osacc-conf.sh /etc/
+cp osacc.conf /etc/
+#cp init_acc.py osacc_functions.py send_acc.py update_acc.py /usr/local/bin/
+#cp osacc-conf.sh /etc/
