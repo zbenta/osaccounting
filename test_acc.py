b86e531014ec37e746abad991a5eac36b155da42
mariojmdavid <mariojmdavid@gmail.com>
2017-11-24 11:35:38 +0000

    fix type error, test

--- test_acc.py
+++ test_acc.py
@@ -34,11 +34,12 @@ if __name__ == '__main__':
             continue
 
         proj = p[0]
+        pname = proj['['name']']
         #a[proj]['vcpus'][idx_start:idx_end] = a[proj]['vcpus'][idx_start:idx_end] + inst['vcpus']
         #a[proj]['mem_mb'][idx_start:idx_end] = a[proj]['mem_mb'][idx_start:idx_end] + inst['memory_mb']
         #a[proj]['disk_gb'][idx_start:idx_end] = a[proj]['disk_gb'][idx_start:idx_end] + inst['root_gb']
         #a[proj]['ninstances'][idx_start:idx_end] = a[proj]['ninstances'][idx_start:idx_end] + 1
-        print proj, type(a[proj]['vcpus'])
+        print pname, type(a[pname]['vcpus'])
         if inst['network_info']:
             print 80 * '='
             n_ips = inst['network_info'][0]['network']['subnets']['ips']['floating_ips'].size
