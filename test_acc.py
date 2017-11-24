84d94108ec352a70669859bc8069f7bd86f47a67
mariojmdavid <mariojmdavid@gmail.com>
2017-11-24 10:33:34 +0000

    check how to fix TypeError: unhashable type: 'list'

--- test_acc.py
+++ test_acc.py
@@ -34,11 +34,12 @@ if __name__ == '__main__':
             continue
 
         proj = p[0]
-        a[p]['vcpus'][idx_start:idx_end] = a[p]['vcpus'][idx_start:idx_end] + inst['vcpus']
-        a[p]['mem_mb'][idx_start:idx_end] = a[p]['mem_mb'][idx_start:idx_end] + inst['memory_mb']
-        a[p]['disk_gb'][idx_start:idx_end] = a[p]['disk_gb'][idx_start:idx_end] + inst['root_gb']
-        a[p]['ninstances'][idx_start:idx_end] = a[p]['ninstances'][idx_start:idx_end] + 1
+        #a[p]['vcpus'][idx_start:idx_end] = a[p]['vcpus'][idx_start:idx_end] + inst['vcpus']
+        #a[p]['mem_mb'][idx_start:idx_end] = a[p]['mem_mb'][idx_start:idx_end] + inst['memory_mb']
+        #a[p]['disk_gb'][idx_start:idx_end] = a[p]['disk_gb'][idx_start:idx_end] + inst['root_gb']
+        #a[p]['ninstances'][idx_start:idx_end] = a[p]['ninstances'][idx_start:idx_end] + 1
 
+        print type(a[p]['vcpus'])
         if inst['network_info']:
             print 80 * '='
             pprint.pprint(inst['network_info'])
