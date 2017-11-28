a9c7333b6791202f0706ce0127b6fa5217f274fb
mariojmdavid <mariojmdavid@gmail.com>
2017-11-28 16:29:31 +0000

    test to fix indexes

--- update_acc.py
+++ update_acc.py
@@ -30,15 +30,20 @@ if __name__ == '__main__':
     state = "upd"  # state is either "init" if first time accounting or "upd"
     projects_in = list()  # fill list of project ID when processing instances or volumes
     array_metrics = dict()  # array with metrics for each project
-    process_inst(di, time_array_all, array_metrics, projects_in, state)
-    process_vol(di, time_array_all, array_metrics, projects_in, state)
-    p_dict = get_projects(di, state)
+    process_inst(df, time_array_all, array_metrics, projects_in, state)
+    process_vol(df, time_array_all, array_metrics, projects_in, state)
+    p_dict = get_projects(df, state)
     with h5py.File(filename, 'r+') as f:
         ts = f['date'][:]
         idx_start = time2index(di, time_array_all)
         idx_end = time2index(df, time_array_all) + 1
         idx_start_ds = time2index(di, ts)
         idx_end_ds = time2index(df, ts) + 1
+        print 80*"="
+        print "idx_start = ", idx_start
+        print "idx_end = ", idx_end
+        print "idx_start_ds = ", idx_start_ds
+        print "idx_end_ds = ", idx_end_ds
         for proj_id in projects_in:
             grp_name = p_dict[proj_id][0]
             if grp_name not in proj_hdf:
