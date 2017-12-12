efdb6c1b79b1b0d8b21ca8bcfcf494c980c612bc
mariojmdavid <mariojmdavid@gmail.com>
2017-12-12 10:16:44 +0000

    add config options for influxDB, and function to connect to InfluxDB

--- test_acc.py
+++ test_acc.py
@@ -5,16 +5,10 @@
 # Author: Mario David <mariojmdavid@gmail.com>
 #
 
-from influxdb import InfluxDBClient
 from osacc_functions import *
 
 if __name__ == '__main__':
     ev = get_conf()
-    dbhost = ev['dbhost']
-    dbport = ev['dbport']
-    dbuser = ev['dbuser']
-    dbpass = ev['dbpass']
-    dbname = ev['dbname']
-    client = InfluxDBClient(dbhost, dbport, dbuser, dbpass, dbname, ssl=True, verify_ssl=False)
+    client = get_influxclient()
     result = client.query('SHOW DATABASES;')
     print("Result: {0}".format(result))
