b9c2b3e5e470c76b0f867c3d5348f22f03dc52e2
mariojmdavid <mariojmdavid@gmail.com>
2020-08-21 15:53:42 +0100

    cloud accounting weekly

--- cloud_acc_weekly.sh
+++ cloud_acc_weekly.sh
@@ -2,20 +2,31 @@
 OSCRED=admin.sh
 PROJ=/var/log/osacc/proj.txt
 
-date
+DATE_END=`date +%Y%m%d -d "last Sun"`
+DATE_START=`date +%m%d -d "last Sun -7 days"`
+BASE_FILE="/var/log/osacc/CLOUD_STRATUS_"
+FILE_USAGE="${BASE_FILE}${DATE_END}${DATE_START}_usage.csv"
+
+ACC_END=`date +%Y-%m-%d -d "last Sun"`
+ACC_START=`date +%Y-%m-%d -d "last Sun -7 days"`
+
+echo "Start: ${DATE_START}"
+echo "End: ${DATE_END}"
+echo "Accounting Start: ${ACC_START}"
+echo "Accounting End: ${ACC_END}"
+echo "Usage file: ${FILE_USAGE}"
+
 mkdir -p /var/log/osacc
 source ${OSCRED}
 
 echo "Get all projects"
-#openstack project list --format value -c Name > ${PROJ}
+openstack project list --format value -c Name > ${PROJ}
 
-
+echo "Account,CPU Hours,Disk GB-Hours,RAM MB-Hours,Servers" > ${FILE_USAGE}
 
 for proj in `cat ${PROJ}`
 do
-    eval "$(openstack usage show --project $proj --format shell)"
     echo "---------------"
-    echo "$proj,$cpu_hours,$disk_gb_hours,$ram_mb_hours,$servers"
+    eval "$(openstack usage show --project $proj --format shell --start ${ACC_START} --end ${ACC_END})"
+    echo "$proj,$cpu_hours,$disk_gb_hours,$ram_mb_hours,$servers" >> ${FILE_USAGE}
 done
-
-DATE=`date +%F_%H-%M`
