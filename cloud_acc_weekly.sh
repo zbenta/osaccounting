3ffc5f458de50542e723afa42c4901418e11f47d
mariojmdavid <mariojmdavid@gmail.com>
2020-08-24 11:56:14 +0100

    remove echo

--- cloud_acc_weekly.sh
+++ cloud_acc_weekly.sh
@@ -30,7 +30,6 @@ do
   then
     continue
   else
-    echo "---------------"
     eval "$(openstack usage show --project $proj --format shell --start ${ACC_START} --end ${ACC_END})"
     echo "$proj,$cpu_hours,$disk_gb_hours,$ram_mb_hours,$servers" >> ${FILE_USAGE}
   fi
