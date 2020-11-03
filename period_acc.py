69431270c7cf344792ef15899bf65ae1956e9913
mariojmdavid <mariojmdavid@gmail.com>
2020-11-03 18:17:02 +0000

    wip

--- period_acc.py
+++ period_acc.py
@@ -13,6 +13,7 @@ import datetime
 import dateutil.parser
 import h5py
 import osacc_functions as oaf
+from dateutil.relativedelta import *
 
 
 if __name__ == '__main__':
@@ -22,24 +23,28 @@ if __name__ == '__main__':
     print(80 * '=')
     print('Filename:', filename)
     my_ini = datetime.datetime(ev['year_ini'], ev['month_ini'], 1, 0, 0, 0)
-    today = datetime.date.today()
-    first = today.replace(day=1)
-    last_month = first - datetime.timedelta(days=1)
+    last_month = datetime.datetime.now()
+    last_month = last_month + relativedelta(months=-1)
+    last_month = last_month + relativedelta(day=31)
     with h5py.File(filename, 'r') as f:
         while(my_ini <= last_month):
             print(80*'+')
-            my_end = my_ini.month + 1
+            my_end = my_ini + relativedelta(months=+1)
             ti = oaf.to_secepoc(my_ini)
             tf = oaf.to_secepoc(my_end)
             ts = f['date'][:]
             idx_start = oaf.time2index(ev, ti, ts)
             idx_end = oaf.time2index(ev, tf, ts)
             print(my_ini, my_end)
-            print(ti, ts)
+            print(ti, tf)
             print(idx_start, idx_end)
             my_ini = my_end
 
-            # for group in f:
-            #     if group == "date":
-            #         continue
-            #     dgroup = f[group]
+            for group in f:
+                if group == "date":
+                    continue
+                dgroup = f[group]
+                for i in range(idx_start, idx_end):
+                    for mtr in oaf.METRICS:
+                        data = dgroup[mtr]
+                        make_mtr = make_mtr + mtr + "=" + str(data[i]) + ","
